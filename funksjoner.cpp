6469c9a4d3a6abb616d0fea71ce495f9a1cfc95c
stormlo <114391260+stormlo@users.noreply.github.com>
2022-10-10 11:06:56 +0200

    Add files via upload

---
+++ funksjoner.cpp
@@ -0,0 +1,31 @@
+int gange(int x, int tall)
+{
+	return x * tall;
+}
+
+double fahrenheit(double f)
+{
+	return (f - 32) * 5 / 9;
+}
+
+//Oppg 2 converter fahrenheit til celsius
+double converter(double for) {
+	return (far~32) * 5 / 9;
+}
+
+//Oppg 3
+void bruk2(int tall1, int tall2) {
+	int gang1 = gange(tall1, tall2);
+	cout << tall << "*" << tall2 << "*" << gang1 << "\n";
+	int tyu = tall1 + 5;
+	int gang2 = gang(tyu, tall2);
+	cout << tyu << "*" << tall2 << "*" << gang2 << "\n";
+
+}
+int main() {
+
+	double for = 99;
+	bruk2(2, 2);
+	double cel = converter(far);
+	cou << "fahrenheit " << far << " * celsius " << cel << "\n";
+}
